79dc3bea189580860ef62984c6199171a4c77e84
SezSahin <sezg0006@edu.eal.dk>
2017-10-02 11:01:56 +0200

    Add project files.

---
+++ Ex09-Timer/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ex09_Timer
+{
+    internal class Timer : TimerTests
+    {
+        public Timer()
+        {
+            
+        }
+
+        
+        public override string ToString()
+        {
+            int minutes = Seconds/60;
+            int hours = minutes / 60;
+
+            minutes = minutes % 60;
+            Seconds = Seconds % 60;
+
+            return "" + Padded(hours) + ":" + Padded(minutes) + ":" + Padded(Seconds);
+
+            //Skrevet uden Padded
+            //return "" + hours.ToString().PadLeft(2, '0') + ":" + minutes.ToString().PadLeft(2, '0') + ":" + Seconds.ToString().PadLeft(2, '0');
+        }
+        public int Seconds { get; internal set; }
+
+        internal string Padded(int v)
+        {
+            return "" + v.ToString().PadLeft(2, '0');
+        }
+    }
+}
